fc0b98631f7ac0579ac6b7339e85dc879a25c065
Rong <rongrubaiya@gmail.com>
2025-10-16 11:35:32 +0600

    banner section done

---
+++ src/Components/card/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <div className='flex flex-col justify-center text-center my-9'>
+        <h1 className='text-4xl font-semibold mb-4'>Trending Apps</h1>
+        <p className='text-gray-400'>Explore All Trending Apps on the Market developed by us</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
+import Card from '../../Components/card/Card';
 
 const Home = () => {
   return (
     <div className='bg-[#FAFAFA] py-20 w-full '>
      <Banner></Banner>
+     <Card></Card>
     </div>
   );
 };
